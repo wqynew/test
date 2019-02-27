979b28f43d4277487e41ab16cd17109222fb7cb1
wqynew <qy_wu@yahoo.com>
2019-02-27 16:05:15 -0500

    The third

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ Created on Wed Feb 27 14:06:42 2019
 @author: wuqia
 """
 import argparse
+import sys
 import os
     
 if __name__ == '__main__':
@@ -12,4 +13,5 @@ if __name__ == '__main__':
     parser.add_argument('ite',type=int,default=5)
     args = parser.parse_args()
     args.ite=args.ite+1
+    sys.stdout.write(args.ite)
     print (args.ite)
