494725f0e7c01a3f015d376b0ba87dc5317528c4
wqynew <qy_wu@yahoo.com>
2019-02-27 17:10:16 -0500

    The third

--- test.py
+++ test.py
@@ -8,10 +8,38 @@ import argparse
 import sys
 import os
     
-if __name__ == '__main__':
+import multiprocessing as mp
+import threading as td
+import time
+
+def job(q,c):
+    res = c
+    for i in range(2):
+        res += i+i**2+i**3
+    q.put(res)
+    
+def multcore(c):
+    q = mp.Queue()
+    #要把q放入参数中,若只有q一个参数，一定要加逗号，否则会报错
+    p1 = mp.Process(target=job,args=(q,c,))
+    p2 = mp.Process(target=job,args=(q,c,))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+    res1 = q.get()
+    res2 = q.get()
+    print('multicore:',res1+res2)
+
+if __name__=='__main__':
     parser = argparse.ArgumentParser(description=os.path.basename(__file__))
     parser.add_argument('ite',type=int,default=5)
     args = parser.parse_args()
     args.ite=args.ite+1
-    sys.stdout.write(args.ite)
+    sys.stdout.write(str(args.ite))
     print (args.ite)
+    st = time.time()
+    multcore(args.ite)
+    st3 = time.time()
+    print('multicode time:',st3 - st)
+
